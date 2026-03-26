db092c2789cabcccbf5e2e5a5396c159f149e47f
elizar <elizar@abyss.wow>
2026-03-26 17:18:43 +0300

    Added tables

--- AppSec/appsec_stand/basket/app-25-03.py
+++ AppSec/appsec_stand/basket/app-25-03.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from database import get_user_by_username, create_user, get_password_by_password_hash, get_or_create_dialog, add_message_to_dialog, get_dialog_messages, get_admin_dialogs, close_dialog, get_dialog_info
-from session_manager import create_session, get_session_data
 import json
 import os
 
@@ -9,13 +7,24 @@ import unicodedata                  # нормализует Unicode
 from urllib.parse import unquote    # декодирует URL
 import bleach                       # Санитизация
 import re                           # Валидация
+#from flask import escape            # Энкодинг - старая версия
 from markupsafe import escape, Markup   # Энкодинг - новая версия
 
 app = Flask(__name__)
 app.secret_key = '6G6A906SBHP7@J0KX0'  #  — заменить 
 
 DATA_FILE = os.path.join('data', 'users.json')
-    
+
+def load_users():
+    if not os.path.exists(DATA_FILE) or os.path.getsize(DATA_FILE) == 0:
+        return {}
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def save_users(users):
+    with open(DATA_FILE, 'w', encoding='utf-8') as f:
+        json.dump(users, f, ensure_ascii=False, indent=2)
+
 @app.route('/')
 def index():
     if 'username' in session:
@@ -31,21 +40,16 @@ def register():
         if not username or not password or not email:
             flash('Заполните все поля', 'error')
             return render_template('register.html')
-        exist_user = get_user_by_username(username)
-        if exist_user:
+        users = load_users()
+        if username in users:
             flash('Пользователь уже существует', 'error')
             return render_template('register.html')
         # Хешируем пароль
         pw_hash = generate_password_hash(password)
-        
-        # Регистрируем пользователя        
-        if create_user(username, email, pw_hash):
-            flash('Регистрация прошла успешно. Войдите.', 'success')
-            return redirect(url_for('login'))
-        else:
-            flash('Ошибка при регистрации', 'error')
-            return render_template('register.html')
-        
+        users[username] = {"email": email, "password": pw_hash}
+        save_users(users)
+        flash('Регистрация прошла успешно. Войдите.', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -56,51 +60,86 @@ def login():
         if not username or not password:
             flash('Заполните все поля', 'error')
             return render_template('login.html')
-        exist_user = get_user_by_username(username)
-        if exist_user and check_password_hash(get_password_by_password_hash(username), password):
+        users = load_users()
+        user = users.get(username)
+        if user and check_password_hash(user['password'], password):
             session['username'] = username
             flash('Вы успешно вошли', 'success')
+            msgs = session.get('messages', [])
+            msgs.append({'text': f'<strong>Admin: </strong> Добро пожаловать в чат поддержки!', 'type': 'admin'})
+            session['messages'] = msgs
+            
+            print("user:", user)
+            print("check_password_hash(user['password']:", check_password_hash(user['password'], password))
+            print("password:", password)
+        
             return redirect(url_for('chat'))
         flash('Неверный логин или пароль', 'error')
         return render_template('login.html')
     return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Вы вышли', 'info')
+    return redirect(url_for('login'))
+
 @app.route('/chat', methods=['GET'])
 def chat():
     if 'username' not in session:
         return redirect(url_for('login'))
-    
-    username = session['username']
-    user = get_user_by_username(username)
-    user_id = user[0]
+    return render_template('chat.html', username=session['username'], messages=session.get('messages', []))
 
-    # Получаем диалог пользователя (создаётся, если нет)
-    dialog_id = get_or_create_dialog(user_id)
-    
-    messages = get_dialog_messages(dialog_id)
-    return render_template('chat.html', username=username, messages=messages, dialog_id=dialog_id)
-                         
 @app.route('/send', methods=['POST'])
 def send():
     if 'username' not in session:
         return redirect(url_for('login'))
-    #message = request.form.get('message', '').strip()
     message = request.form.get('message', '')
     print("Message 1 :", message)
-    clean_message = message
-
-    # Декодирование, нормализация, санитизация
-    decoded = unquote(message)      # декодирует URL
-    normalized = unicodedata.normalize('NFC', decoded)  # нормализует unicode
-    clean_message = bleach.clean(normalized)  # санитизация
-    print("Message 3 :", clean_message)
-
+    clean = message
+    # Санитизация
+    '''clean = bleach.clean(
+        message,
+        #tags=["b", "i", "u"],
+        attributes={},
+        strip=True
+    )
+    clean = bleach.clean(message)
+    #clean = message
+    print("Message 2 :", clean)
+    '''
+    # декодирует Нормализация Санитизация
+    '''decoded = unquote(message)      # декодирует URL
+    normalized = unicodedata.normalize('NFC', decoded)  # нормализует Unicode
+    clean = bleach.clean(normalized)  # Санитизация
+    print("Message 3 :", clean)
+    '''
+    # Валидация
+    '''if not re.match(r"^[a-zA-Z0-9\s]+$", message):
+        flash('Недопустимые символы (<,>,(,),:,;,.,, и т.д.)', 'error')
+        return redirect(url_for('chat'))
+        #return "Invalid input"
+    clean = message
+    print("Message 3 :", clean)
+    '''
+    # "Энкодинг    
+    '''clean = escape(message)  # Безопасное экранирование
+    print("Message 3 :", clean)
+    '''
+    # Белый список
+    '''white_list = ["b", "i", "a"]  # разрешённые HTML-теги
+    clean = bleach.clean(
+        message,
+        tags = white_list
+    )
+    print("Message 3 :", clean)
+    '''
+    # сохранить сообщение в сессии (не для продакшна). В учебном стенде можно хранить сообщения в памяти или в файле
+    msgs = session.get('messages', [])
     username = session['username']
-    user = get_user_by_username(username)
-    user_id = user[0]
-    dialog_id = get_or_create_dialog(user_id)
-    
-    add_message_to_dialog(dialog_id, user_id, clean_message)
+    #msgs.append({"user": session['username'], "text": message})
+    msgs.append({'text': f'<strong>{username}:</strong> {clean}', 'type': 'user'})
+    session['messages'] = msgs
     return redirect(url_for('chat'))
     
 # Для демонстрации сохраненной XSS - <script>alert(1)</script>
@@ -132,45 +171,5 @@ def send():
     session['messages'] = msgs
     return redirect(url_for('chat'))
 '''
-
-# Панель администратора
-@app.route('/admin/dialogs')
-#@login_required
-def admin_dialogs():
-    # Проверяем, что пользователь — администратор
-    if g.current_user.role != 'admin':
-        abort(403)
-
-    admin_id = g.current_user.id
-    dialogs = get_admin_dialogs(admin_id)
-    return render_template('admin_dialogs.html', dialogs=dialogs)
-
-# Просмотр диалога администратором
-@app.route('/admin/dialog/<int:dialog_id>')
-#@login_required
-def admin_view_dialog(dialog_id):
-    # Проверяем, что текущий пользователь — администратор
-    if g.current_user_role != 'admin':
-        abort(403)  # Запрет доступа
-
-    # Получаем информацию о диалоге
-    dialog = get_dialog_info(dialog_id)
-    if not dialog:
-        abort(404)  # Диалог не найден
-
-    # Получаем все сообщения диалога
-    messages = get_dialog_messages(dialog_id)
-
-    return render_template('admin_dialog.html', dialog=dialog, messages=messages)
-    
-@app.route('/logout', methods=['POST'])
-def logout():
-    #if request.form.get('csrf_token') != session.get('csrf_token'):
-    #    abort(403)  # Запретить доступ при неверном токене
-    #session.pop('username', None)
-    session.clear()
-    flash('Вы вышли', 'info')
-    return redirect(url_for('login'))
-    
 if __name__ == '__main__':
     app.run(debug=True)
